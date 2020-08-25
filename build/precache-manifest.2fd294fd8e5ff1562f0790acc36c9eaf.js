04713b9dc14261e0ab3a840e8635ca9c5a4b922c
iamNarcisse <mechikaegon@gmail.com>
2020-08-25 15:49:22 +0100

    build updated

---
+++ build/precache-manifest.2fd294fd8e5ff1562f0790acc36c9eaf.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81f7ba7bc9e1284af67d151a1bf86d3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54d7dc9a20394041989a",
+    "url": "/static/css/2.2ea6df07.chunk.css"
+  },
+  {
+    "revision": "a0282473db87b3ff9e40",
+    "url": "/static/css/main.80f65a8b.chunk.css"
+  },
+  {
+    "revision": "54d7dc9a20394041989a",
+    "url": "/static/js/2.327e7545.chunk.js"
+  },
+  {
+    "revision": "253d0d72fd1c6eb1e9606c0cb98dca11",
+    "url": "/static/js/2.327e7545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0282473db87b3ff9e40",
+    "url": "/static/js/main.36e0b840.chunk.js"
+  },
+  {
+    "revision": "6a112c6404f7692e4d97",
+    "url": "/static/js/runtime-main.ac8ece2d.js"
+  },
+  {
+    "revision": "e55232b88a69e4ac1be5691dd1d9347e",
+    "url": "/static/media/IPF_Logo.e55232b8.png"
+  },
+  {
+    "revision": "1ab0ead8e40b5a6cc120fd0c1a8b988c",
+    "url": "/static/media/Indian_Logo.1ab0ead8.png"
+  },
+  {
+    "revision": "5125a8de5094e99993ed18c86a726c24",
+    "url": "/static/media/Nigeria_Logo.5125a8de.png"
+  },
+  {
+    "revision": "4ac5e2c55333d6da61134412106fc7b9",
+    "url": "/static/media/bg01.4ac5e2c5.png"
+  },
+  {
+    "revision": "cab8a4368525268e899399140277d484",
+    "url": "/static/media/bg02.cab8a436.png"
+  },
+  {
+    "revision": "e93c108611b8c191a895fdc09b953755",
+    "url": "/static/media/calendar.e93c1086.png"
+  },
+  {
+    "revision": "b027e989afda87e67e9d399501da9e2e",
+    "url": "/static/media/landing.b027e989.png"
+  },
+  {
+    "revision": "1daf6b4997b0333d810fcc7b49eaf04d",
+    "url": "/static/media/loader.1daf6b49.svg"
+  },
+  {
+    "revision": "47523bf2d51e11678f795da4dca7ccc7",
+    "url": "/static/media/medal.47523bf2.png"
+  },
+  {
+    "revision": "6f268c08869cd97f4a503224b490e670",
+    "url": "/static/media/report.6f268c08.png"
+  },
+  {
+    "revision": "5831ca9697ede6a82b0e0c1973d7f09f",
+    "url": "/static/media/slide-1.5831ca96.jpg"
+  },
+  {
+    "revision": "443c4c430cd1d6b5ba8ba2b3abae8413",
+    "url": "/static/media/slide-2.443c4c43.jpg"
+  },
+  {
+    "revision": "f83cc6d48bd17e6e43650b1e8a6ed677",
+    "url": "/static/media/slide-3.f83cc6d4.jpg"
+  },
+  {
+    "revision": "f204660cc5afa7a18fdcb107029b514a",
+    "url": "/static/media/slide-4.f204660c.jpg"
+  }
+]);
