e51742d9982f00c05798f5379239092eba2abc94
oluwaseun <omoniyi22>
2020-10-05 12:48:25 +0100

    progress

---
+++ src/components/Forms/Event.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class EventForm extends Component {
+  render() {
+    return (
+      <div>
+        EventForm
+      </div>
+    )
+  }
+}
+
+export default EventForm
